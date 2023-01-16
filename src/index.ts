016723e9762cb7a078dd388ad996f90a11e37883
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-01-16 19:55:38 +0545

    fix: add lint and format

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import axios from 'axios';
+import axios from 'axios'
 
-
-
-type providers = 'isgd' | 'cdpt' | 'vgd' | '4hnet' | 'tinube' | 'rbgy' | 'vurl' | 'haha' | 'pwm' | 'cya' | 'hideuri' | 'tnyim';
+type providers =
+| 'isgd'
+| 'cdpt'
+| 'vgd'
+| '4hnet'
+| 'tinube'
+| 'rbgy'
+| 'vurl'
+| 'haha'
+| 'pwm'
+| 'cya'
+| 'hideuri'
+| 'tnyim'
 
 /**
  *
@@ -10,8 +20,8 @@ type providers = 'isgd' | 'cdpt' | 'vgd' | '4hnet' | 'tinube' | 'rbgy' | 'vurl' 
  * @interface IResponse
  */
 interface IResponse {
-	longUrl: string;
-	shortUrl: string;
+  longUrl: string
+  shortUrl: string
 }
 
 /**
@@ -20,8 +30,8 @@ interface IResponse {
  * @interface IOptions
  */
 interface IOptions {
-	provider: providers;
-	timeout?: number;
+  provider: providers
+  timeout?: number
 }
 
 /**
@@ -31,13 +41,11 @@ interface IOptions {
  * @interface IProviders
  */
 interface IProviders {
-	url: string;
-	method: string;
-	body?: any;
-	formData?: boolean;
+  url: string
+  method: string
+  body?: any
+  formData?: boolean
 }
-
-
 
 /**
  *
@@ -46,40 +54,28 @@ interface IProviders {
  * @param {string} longUrl
  * @return {*}  {IResponse}
  */
-function responseMap(response: Record<any, any>, longUrl: string,provider: providers): IResponse {
-	const responseType = response.headers['content-type'].split(';')[0];
-	if (!['text/plain', 'text/html'].includes(responseType)) {
-		if (response.data?.url) {
-			return { longUrl, shortUrl: response.data.url };
-		} else if (response.data?.shortUrl) {
-			return { longUrl, shortUrl: response.data.shortUrl };
-		} else if (response.data?.shortenedUrl) {
-			return { longUrl, shortUrl: response.data.shortenedUrl };
-		} else if (response.data?.short) {
-			return { longUrl, shortUrl: response.data.short };
-		} else if (response.data?.short_url) {
-			return { longUrl, shortUrl: response.data.short_url };
-		} else if (response.data?.result_url) {
-			return { longUrl, shortUrl: response.data.result_url };
-		}
-	}
+function responseMap(response: Record<any, any>, longUrl: string, provider: providers): IResponse {
+  const responseType = response.headers['content-type'].split(';')[0]
+  if (!['text/plain', 'text/html'].includes(responseType)) {
+    if (response.data?.url)
+      return { longUrl, shortUrl: response.data.url }
+    else if (response.data?.shortUrl)
+      return { longUrl, shortUrl: response.data.shortUrl }
+    else if (response.data?.shortenedUrl)
+      return { longUrl, shortUrl: response.data.shortenedUrl }
+    else if (response.data?.short)
+      return { longUrl, shortUrl: response.data.short }
+    else if (response.data?.short_url)
+      return { longUrl, shortUrl: response.data.short_url }
+    else if (response.data?.result_url)
+      return { longUrl, shortUrl: response.data.result_url }
+  }
 
-	if(['haha','pwm','cya'].includes(provider)){
+  if (['haha', 'pwm', 'cya'].includes(provider))
+    return { longUrl, shortUrl: `https://${provider}.se/${response.data}` }
 
-
-		return { longUrl, shortUrl: `https://${provider}.se/${response.data}` };
-
-	}
-
-
-		return { longUrl, shortUrl: response.data };
-
-
-
-
+  return { longUrl, shortUrl: response.data }
 }
-
-
 
 /**
  *
@@ -89,83 +85,84 @@ function responseMap(response: Record<any, any>, longUrl: string,provider: provi
  * @return {*}
  */
 const getAxios = (provider: IProviders, longUrl: string, option: IOptions) => {
-	if (provider.method === 'get') {
-		return axios.get(provider.url + longUrl, { timeout: option.timeout });
-	} else {
-		return axios.post(provider.url, provider.body(longUrl), {
-			timeout: option.timeout,
-		});
-	}
-};
+  if (provider.method === 'get') {
+    return axios.get(provider.url + longUrl, { timeout: option.timeout })
+  }
+  else {
+    return axios.post(provider.url, provider.body(longUrl), {
+      timeout: option.timeout,
+    })
+  }
+}
 
 const ValidProviders: Record<string, IProviders> = {
-	isgd: {
-		url: 'https://is.gd/create.php?format=simple&url=',
-		method: 'get',
-	},
-	cdpt: {
-		url: 'https://cdpt.in/shorten?url=',
-		method: 'get',
-	},
-	vgd: {
-		url: 'https://v.gd/create.php?format=simple&url=',
-		method: 'get',
-	},
-	haha: {
-		url: 'https://www.haha.se/lank.php?dataLank=',
-		method: 'get',
-	},
+  'isgd': {
+    url: 'https://is.gd/create.php?format=simple&url=',
+    method: 'get',
+  },
+  'cdpt': {
+    url: 'https://cdpt.in/shorten?url=',
+    method: 'get',
+  },
+  'vgd': {
+    url: 'https://v.gd/create.php?format=simple&url=',
+    method: 'get',
+  },
+  'haha': {
+    url: 'https://www.haha.se/lank.php?dataLank=',
+    method: 'get',
+  },
 
-	cya: {
-		url: 'https://www.haha.se/lank.php?dataLank=',
-		method: 'get',
-	},
+  'cya': {
+    url: 'https://www.haha.se/lank.php?dataLank=',
+    method: 'get',
+  },
 
-	pwm: {
-		url: 'https://www.haha.se/lank.php?dataLank=',
-		method: 'get',
-	},
+  'pwm': {
+    url: 'https://www.haha.se/lank.php?dataLank=',
+    method: 'get',
+  },
 
-	tnyim: {
-		url: 'http://tny.im/yourls-api.php?action=shorturl&format=simple&url=',
-		method: 'get',
-	},
+  'tnyim': {
+    url: 'http://tny.im/yourls-api.php?action=shorturl&format=simple&url=',
+    method: 'get',
+  },
 
-	'4hnet': {
-		url: 'https://4h.net/api/?url=',
-		method: 'get',
-	},
+  '4hnet': {
+    url: 'https://4h.net/api/?url=',
+    method: 'get',
+  },
 
-	vurl: {
-		url: 'https://vurl.com/api.php?url=',
-		method: 'get',
-	},
-	// POST APIS
+  'vurl': {
+    url: 'https://vurl.com/api.php?url=',
+    method: 'get',
+  },
+  // POST APIS
 
-	tinube: {
-		url: 'https://api.tinu.be/api/shorten',
-		method: 'post',
-		body: (val: string) => {
-			return { longUrl: val };
-		},
-	},
+  'tinube': {
+    url: 'https://api.tinu.be/api/shorten',
+    method: 'post',
+    body: (val: string) => {
+      return { longUrl: val }
+    },
+  },
 
-	hideuri: {
-		url: 'https://hideuri.com/api/v1/shorten',
-		method: 'post',
-		body: (val: string) => {
-			return { url: val };
-		},
-	},
+  'hideuri': {
+    url: 'https://hideuri.com/api/v1/shorten',
+    method: 'post',
+    body: (val: string) => {
+      return { url: val }
+    },
+  },
 
-	rbgy: {
-		url: 'https://free-url-shortener.rb.gy/shorten',
-		method: 'post',
-		body: (val: string) => {
-			return { destination: val, "dryRun": true }
-		},
-	},
-};
+  'rbgy': {
+    url: 'https://free-url-shortener.rb.gy/shorten',
+    method: 'post',
+    body: (val: string) => {
+      return { destination: val, dryRun: true }
+    },
+  },
+}
 
 /**
  *
@@ -175,21 +172,12 @@ const ValidProviders: Record<string, IProviders> = {
  * @param {IOptions} option
  * @return {IResponse}
  */
-const minify =  async (
-	longUrl: string,
-	option: IOptions = { provider: 'isgd', timeout: 2000 },
+const minify = async (
+  longUrl: string,
+  option: IOptions = { provider: 'isgd', timeout: 2000 },
 ): Promise<IResponse> => {
-	try {
-		const response = await getAxios(
-			ValidProviders[option.provider],
-			longUrl,
-			option,
-		);
-		return responseMap(response, longUrl, option.provider);
-	} catch (error) {
-		throw error;
-	}
-};
+  const response = await getAxios(ValidProviders[option.provider], longUrl, option)
+  return responseMap(response, longUrl, option.provider)
+}
 
-
-export default minify;
+export default minify
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,68 +1,62 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest'
 
-import minify from '../src/index';
+import minify from '../src/index'
 
-const url = 'https://rubiin.vercel.app';
-
-
+const url = 'https://rubiin.vercel.app'
 
 describe('should get a shortened url', () => {
-	it('should get shortened url with is.gd', async () => {
-		const response = await minify(url, { provider: 'isgd' });
-		expect(response.shortUrl).toMatch(/^https:\/\/is.gd\//);
-	});
+  it('should get shortened url with is.gd', async () => {
+    const response = await minify(url, { provider: 'isgd' })
+    expect(response.shortUrl).toMatch(/^https:\/\/is.gd\//)
+  })
 
+  it('should get shortened url with v.gd', async () => {
+    const response = await minify(url, { provider: 'vgd' })
+    expect(response.shortUrl).toMatch(/^https:\/\/v.gd\//)
+  })
 
-	it('should get shortened url with v.gd', async () => {
-		const response = await minify(url, { provider: 'vgd' });
-		expect(response.shortUrl).toMatch(/^https:\/\/v.gd\//);
-	});
+  it('should get shortened url with 4h.net', async () => {
+    const response = await minify(url, { provider: '4hnet' })
+    expect(response.shortUrl).toMatch(/^https:\/\/4h.net\//)
+  })
 
-		it('should get shortened url with 4h.net', async () => {
-		const response = await minify(url, { provider: '4hnet' });
-			expect(response.shortUrl).toMatch(/^https:\/\/4h.net\//);
-	});
+  it('should get shortened url with hideuri.com', async () => {
+    const response = await minify(url, { provider: 'hideuri' })
+    expect(response.shortUrl).toMatch(/^https:\/\/hideuri.com\//)
+  })
 
-	it('should get shortened url with hideuri.com', async () => {
-		const response = await minify(url, { provider: 'hideuri' });
-			expect(response.shortUrl).toMatch(/^https:\/\/hideuri.com\//);
-	});
+  it('should get shortened url with tinu.be', async () => {
+    const response = await minify(url, { provider: 'tinube' })
+    expect(response.shortUrl).toMatch(/^https:\/\/tinu.be\//)
+  })
 
-	it('should get shortened url with tinu.be', async () => {
-		const response = await minify(url, { provider: 'tinube' });
-		expect(response.shortUrl).toMatch(/^https:\/\/tinu.be\//);
-	});
+  it('should get shortened url with rb.gy', async () => {
+    const response = await minify(url, { provider: 'rbgy' })
+    expect(response.shortUrl).toMatch(/^https:\/\/rb.gy\//)
+  })
 
-	it('should get shortened url with rb.gy', async () => {
-		const response = await minify(url, { provider: 'rbgy' });
-		expect(response.shortUrl).toMatch(/^https:\/\/rb.gy\//);
-	});
+  it('should get shortened url with vurl.com', async () => {
+    const response = await minify(url, { provider: 'vurl' })
+    expect(response.shortUrl).toMatch(/^https:\/\/vurl.com\//)
+  })
 
-	it('should get shortened url with vurl.com', async () => {
-		const response = await minify(url, { provider: 'vurl' });
-		expect(response.shortUrl).toMatch(/^https:\/\/vurl.com\//);
-	});
+  it('should get shortened url with haha.se', async () => {
+    const response = await minify(url, { provider: 'haha' })
+    expect(response.shortUrl).toMatch(/^https:\/\/haha.se\//)
+  })
 
+  it('should get shortened url with pwm.se', async () => {
+    const response = await minify(url, { provider: 'pwm' })
+    expect(response.shortUrl).toMatch(/^https:\/\/pwm.se\//)
+  })
 
-	it('should get shortened url with haha.se', async () => {
-		const response = await minify(url, { provider: 'haha' });
-		expect(response.shortUrl).toMatch(/^https:\/\/haha.se\//);
-	});
+  it('should get shortened url with cya.se', async () => {
+    const response = await minify(url, { provider: 'cya' })
+    expect(response.shortUrl).toMatch(/^https:\/\/cya.se\//)
+  })
 
-	it('should get shortened url with pwm.se', async () => {
-		const response = await minify(url, { provider: 'pwm' });
-		expect(response.shortUrl).toMatch(/^https:\/\/pwm.se\//);
-	});
-
-
-	it('should get shortened url with cya.se', async () => {
-		const response = await minify(url, { provider: 'cya' });
-		expect(response.shortUrl).toMatch(/^https:\/\/cya.se\//);
-	});
-
-
-	it('should get shortened url with tnyim', async () => {
-		const response = await minify(url, { provider: 'tnyim' });
-		expect(response.shortUrl).toMatch(/^http:\/\/tny.im\//);
-	});
-});
+  it('should get shortened url with tnyim', async () => {
+    const response = await minify(url, { provider: 'tnyim' })
+    expect(response.shortUrl).toMatch(/^http:\/\/tny.im\//)
+  })
+})
